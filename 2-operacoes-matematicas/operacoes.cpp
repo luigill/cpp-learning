5883f13ff7367132a351e2f96b0b060da140f965
Lui Gill Aquini <lgaquini@gmail.com>
2025-05-13 16:27:23 -0300

    1.11

---
+++ 2-operacoes-matematicas/operacoes.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+
+    int sum = 10 + 15; // Soma
+    std::cout << sum << "\n";
+    int sub = 15 - 10; // Subtração
+    std::cout << sub << "\n";
+    int mult = 10 * 5; // Multiplicação
+    std::cout << mult << "\n";
+    int div = 10 / 5; // Divisão
+    std::cout << div << "\n";
+    int modulus = 10 % 5; // Resto da divisão
+    std::cout << modulus << "\n";
+
+    int x = 10;
+
+    int increment = ++x; // Incremento de valor
+    std::cout << increment << "\n";
+    int decrement = --x; // Decremento de valor
+    std::cout << decrement << "\n";
+
+    return 0;
+}
