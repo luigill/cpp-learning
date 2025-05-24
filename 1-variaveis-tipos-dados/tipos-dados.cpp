e82f91a371b099251e5f93ec68f6100f1f4b6324
luigill <lgaquini@gmail.com>
2025-05-24 16:04:17 -0300

    fix error

--- 1-variaveis-tipos-dados/tipos-dados.cpp
+++ 1-variaveis-tipos-dados/tipos-dados.cpp
@@ -6,18 +6,23 @@ int main()
 
     // int = números inteiros
     int x = 10;
+    std::cout << x << "\n";
 
     // float = ponto flutuante em precisão simples
     float pi = 3.14;
+    std::cout << pi << "\n";
 
     // double = ponto flutuante em precisão dupla
     double y = 28888009.8695076;
+    std::cout << y << "\n";
 
     // char = caracter simples
     char letter = 'p';
+    std::cout << letter << "\n";
 
     // bool = Valor Verdadeiro ou Falso
     bool condition = true;
+    std::cout << condition << "\n";
 
     // arrays = Armazenam uma coleção de elementos do mesmo tipo
     int number[5] = {0, 1, 2, 3, 4};
